0b01310721613d780d45de1bf13008051c3a280b
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-24 14:50:28 -0400

    user story 1 complete

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,8 @@
 class Studio < ApplicationRecord
   has_many :movies
+
+  def find_all_movies
+    movies = Movie.all
+    movies = movies.where(studio_id: self.id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    studio_1 = Studio.create(id: 1, name: "Universal Studios", location: "Hollywood")
+    studio_2 = Studio.create(id: 2, name: "Warner Brothers", location: "Burbank")
+    
+    movie_1 = Movie.create(id: 1, studio_id: 1, title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+    movie_2 = Movie.create(id: 2, studio_id: 1,title: 'Indiana Jones', creation_year: 1977, genre: 'Action/Adventure')
+    movie_3 = Movie.create(id: 3, studio_id: 2,title: 'Avengers', creation_year: 2013, genre: 'Action/Adventure')
+    movie_4 = Movie.create(id: 4, studio_id: 2,title: 'Bridget Jones Diary', creation_year: 2001, genre: 'Romance')
