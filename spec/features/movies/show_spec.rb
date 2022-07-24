f8bc7bb678b3df969698947cbdcd132bdb073835
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-24 15:57:11 -0400

    premigration

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -6,13 +6,21 @@ RSpec.describe 'Movies Show Page' do
     studio_2 = Studio.create(id: 200, name: "Warner Brothers", location: "Burbank")
     
     movie_1 = Movie.create(id: 100, title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure', studio_id: studio_1.id)
-    movie_2 = Movie.create(id: 200, title: 'Indiana Jones', creation_year: 1977, genre: 'Action/Adventure', studio_id: studio_1.id)
-    movie_3 = Movie.create(id: 300, title: 'Avengers', creation_year: 2013, genre: 'Action/Adventure', studio_id: studio_2.id)
+    movie_2 = Movie.create(id: 200, title: 'Indiana Jones', creation_year: 1977, genre: 'Fun', studio_id: studio_1.id)
+    movie_3 = Movie.create(id: 300, title: 'Avengers', creation_year: 2013, genre: 'Drama', studio_id: studio_2.id)
     movie_4 = Movie.create(id: 400, title: 'Bridget Jones Diary', creation_year: 2001, genre: 'Romance', studio_id: studio_2.id)
     
     visit "/movies/#{movie_1.id}"
 
+    within "#movie-#{movie_1.id}" do
     expect(page).to have_content(movie_1.title)
+    expect(page).to have_content(movie_1.creation_year)
+    expect(page).to have_content(movie_1.genre)
+    expect(page).to have_no_content(movie_2.title)
+    expect(page).to have_no_content(movie_2.creation_year)
+    expect(page).to have_no_content(movie_2.genre)
+    
+    end
 
   end
 end
