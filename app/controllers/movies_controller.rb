7629205b58eabdbdaec63813c14a8b11ffe64bc7
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-24 16:54:02 -0400

    add user story 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,11 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def create
+    @movie = Movie.find(params[:id])
+    @actor = Actor.create!(name: params[:actor_name], age: params[:actor_age])
+    @movie.actors << @actor
+    redirect_to "/movies/#{@movie.id}"
+  end
+
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -52,6 +52,38 @@ RSpec.describe 'Movies Show Page' do
     expect(page).to have_content("Average Actor Age: 43.5")
     end
   end
+
+    it "sees a form to add an actor to this movie and when form is submitted it is redirected back to movie show page and actor's name appears" do
+    studio_1 = Studio.create(id: 100, name: "Universal Studios", location: "Hollywood")
+    studio_2 = Studio.create(id: 200, name: "Warner Brothers", location: "Burbank")
+    
+    movie_1 = Movie.create(id: 100, title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure', studio_id: studio_1.id)
+    movie_2 = Movie.create(id: 200, title: 'Indiana Jones', creation_year: 1977, genre: 'Fun', studio_id: studio_1.id)
+    movie_3 = Movie.create(id: 300, title: 'Avengers', creation_year: 2013, genre: 'Drama', studio_id: studio_2.id)
+    movie_4 = Movie.create(id: 400, title: 'Bridget Jones Diary', creation_year: 2001, genre: 'Romance', studio_id: studio_2.id)
+
+    actor_1 = Actor.create(id: 100, name: 'Harrison Ford', age: 35)
+    actor_2 = Actor.create(id: 200, name: 'Sean Connery', age: 52)
+    actor_3 = Actor.create(id: 300, name: 'Robert Downey Jr.', age: 33)
+    actor_4 = Actor.create(id: 400, name: 'Scarlett Johansson', age: 27)
+
+    movie_actor_1 = MovieActor.create(movie_id: movie_1.id, actor_id: actor_1.id)
+    movie_actor_2 = MovieActor.create(movie_id: movie_1.id, actor_id: actor_2.id)
+    movie_actor_3 = MovieActor.create(movie_id: movie_2.id, actor_id: actor_3.id)
+    movie_actor_4 = MovieActor.create(movie_id: movie_2.id, actor_id: actor_4.id)
+    
+    visit "/movies/#{movie_1.id}"
+
+      fill_in "actor_name", with: "Tom Cruise"
+      fill_in "actor_age", with: "3"
+
+      click_on "Submit"
+
+      visit "/movies/#{movie_1.id}"
+
+      expect(page).to have_content("Tom Cruise")
+      expect(page).to have_content("Average Actor Age: 30.0")
+  end
 end
 
 
